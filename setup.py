2cbe6a3873664c7fa183eb5027dba20e16389dba
Eti Chiranjeevi <etichiranjeevi125@gmail.com>
2025-06-22 13:13:40 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="CHIRANJEEVI",
+    author_email="etichiranjeevi16@gmail.com",
+    packages=find_packages(),
+
+)
